886400874d5aeecccc599b411bb1d2c7586ac4a5
JasonMDev <jasonmansdev@gmail.com>
2016-03-04 11:04:24 +0000

    Addded error handling for the devise gem in the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def hello
-  	render text: "Hello world!"
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, :alert => exception.message
   end
+  
 end
