524584b4b53ef6c35b90cd807bd8be8323deceac
JasonMDev <jasonmansdev@gmail.com>
2016-03-04 11:00:56 +0000

    Installed and configured the CanCanCan gem for the user model.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, :except => [:show, :index]
+  load_and_authorize_resource
 
   # GET /users
   # GET /users.json
