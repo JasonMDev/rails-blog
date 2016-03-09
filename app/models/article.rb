0539827fd1ba137618612d5b76e1ababcf767739
JasonMDev <jasonmansdev@gmail.com>
2016-03-09 00:11:31 +0000

    Modified the article validation.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,7 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 	validates :title, presence: true,
 	                  length: { minimum: 5}
-  validates :description, presence: true,
-                    length: { maximum: 50}                  
+  validates :description, presence: true              
 
   # Raty average rating
   def average_rating
