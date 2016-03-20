b47ff62fce261f5ac53c52f75b51944e82021b0c
JasonMDev <jasonmansdev@gmail.com>
2016-03-20 21:27:54 +0000

    Added the code for a responsive nav bar.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -12,3 +12,20 @@ $(document).on('ready page:load', function(){
       }
     });
 });
+
+//When the document is ready the following actions will take place.
+$(document).ready(function() {
+  
+  //Creates smooth scrolling effect for menus.
+  var $root = $('html, body');
+  $('.navbar-nav a').click(function() {
+    var href = $.attr(this, 'href');
+    $root.animate({
+      scrollTop: $(href).offset().top
+      }, 500, function () {
+        window.location.hash = href;
+        });
+    return false;
+    });
+  
+});
